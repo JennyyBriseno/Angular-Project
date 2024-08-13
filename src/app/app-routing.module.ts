7a51cfa59387a67cbb1f18f7cc0ced73142dec0d
JennyyBriseno <Jennybriseno12@gmail.com>
2024-08-13 12:10:10 -0500

    Finished with base project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
 
 const routes : Routes = [
   { path: 'home', component: HomeComponent, title: 'Home-joes-robot-shop'},
   { path: 'catalog', component: CatalogComponent, title: 'Catalog-joes-robot-shop'},
   { path: 'cart', component: CartComponent, title: 'Cart-joes-robot-shop'},
-  { path: '', redirectTo: '/home', pathMatch: 'full'}
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'form-controls', component: TemplateFormControlsComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
